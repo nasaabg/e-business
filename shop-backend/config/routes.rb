472a821cc5a520da56e78ee2f37109b2e0fe361e
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2018-06-03 14:46:17 +0200

    Add github auth on backend

--- shop-backend/config/routes.rb
+++ shop-backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/auth/github', to: 'authentication#github', format: false
+
+  resources :users
   namespace :api do
     namespace :v1 do
       resources :products
